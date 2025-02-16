e85fea042b6038cc4243c0bb7d161cae19a289d2
sea gin <cijin@seagin.me>
2025-02-16 19:30:37 +0700

    initialize encode method

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,48 @@
 const std = @import("std");
+const math = std.math;
 
-pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+const EncodeError = error{
+    EmptyInput,
+};
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+const base64 = struct {
+    lookupTable: [64]u8,
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    pub fn init() base64 {
+        const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        const lower = "abcdefghijklmnopqrstuvwxyz";
+        const symbols = "0123456789+/";
 
-    try bw.flush(); // don't forget to flush!
-}
+        return base64{ .lookupTable = upper ++ lower ++ symbols };
+    }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
+    pub fn charAt(self: base64, i: u8) u8 {
+        return self.lookupTable[i];
+    }
+
+    pub fn encode(_: base64, text: []u8) EncodeError![]u8 {
+        if (text.len == 0) {
+            return EncodeError.EmptyInput;
+        }
+
+        var encodedTextLen: usize = 0;
+        if (text.len < 3) {
+            encodedTextLen = 4;
+        } else {
+            encodedTextLen = try math.divCeil(usize, text.len, 3);
+            encodedTextLen * 4;
+        }
+        const encoded: [encodedTextLen]u8 = undefined;
+
+        _ = encoded;
+        // Todo:
+        // Test out with an outbuffer of 4 with input ex:'Lil'
+        // go through 3 bytes at a time
+        // output byte 1: in_byte[0] & b11111100
+        // output byte 2: in_byte[0] & 00000011 << 6 + in_byte[1] >> 2
+        // output byte 3: in_byte[1] & 00000011 << 6 + in_byte[2] >> 2
+        // output byte 4: in_byte[2] & 00000011 << 6 + in_byte[3] >> 2
+    }
+};
+
+pub fn main() !void {}
