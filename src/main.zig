f5bc40c42b9f28ad0dacc749109efd1a32be2754
sea gin <cijin@seagin.me>
2025-02-18 09:11:40 +0700

    base 64 encode complete

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,9 @@
 const std = @import("std");
+const print = std.debug.print;
+const mem = std.mem;
 const math = std.math;
+const heap = std.heap;
+const assert = std.debug.assert;
 
 const EncodeError = error{
     EmptyInput,
@@ -7,24 +11,22 @@ const EncodeError = error{
 };
 
 const base64 = struct {
-    lookupTable: [64]u8,
+    lookupTable: [64:0]u8,
 
     pub fn init() base64 {
         const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         const lower = "abcdefghijklmnopqrstuvwxyz";
         const symbols = "0123456789+/";
 
-        return base64{ .lookupTable = upper ++ lower ++ symbols };
+        return base64{ .lookupTable = upper.* ++ lower.* ++ symbols.* };
     }
 
-    pub fn charAt(self: base64, i: u8) EncodeError!u8 {
-        if (i >= 64) {
-            return EncodeError.NotBase64;
-        }
+    pub fn charAt(self: base64, i: u8) u8 {
+        assert(i < 64);
         return self.lookupTable[i];
     }
 
-    pub fn encode(self: base64, text: []u8) EncodeError![]u8 {
+    pub fn encode(self: base64, allocator: mem.Allocator, text: []const u8) ![]u8 {
         if (text.len == 0) {
             return EncodeError.EmptyInput;
         }
@@ -34,30 +36,69 @@ const base64 = struct {
             encodedTextLen = 4;
         } else {
             encodedTextLen = try math.divCeil(usize, text.len, 3);
-            encodedTextLen * 4;
+            encodedTextLen = encodedTextLen * 4;
         }
-        const encoded: [encodedTextLen]u8 = undefined;
 
-        var isStart: bool = true;
-        for (text, 0..encodedTextLen) |c, i| {
-            if (i % 4 == 0) {
-                if (isStart) {
-                    encoded[i] = try self.charAt(c >> 2);
-                    encoded[i + 1] = c << 4;
-                    isStart = false;
-                } else {
-                    // Todo:
-                    // what to do on the last one?
-                    // When to insert '='?
-                    isStart = true;
-                }
-                continue;
+        const encoded = try allocator.alloc(u8, encodedTextLen);
+        var encodedIdx: usize = 0;
+        var buf = [3]u8{ 0, 0, 0 };
+        var bufIdx: u8 = 0;
+
+        for (text) |c| {
+            buf[bufIdx] = c;
+            bufIdx += 1;
+
+            if (bufIdx == 3) {
+                encoded[encodedIdx] = self.charAt(buf[0] >> 2);
+                encoded[encodedIdx + 1] = self.charAt(((buf[0] & 0x03) << 4) + buf[1] >> 4);
+                encoded[encodedIdx + 2] = self.charAt(((buf[1] & 0x0f) << 2) + buf[2] >> 6);
+                encoded[encodedIdx + 3] = self.charAt((buf[2] & 0x3f));
+
+                encodedIdx += 4;
+                bufIdx = 0;
             }
-
-            encoded[i] = try self.charAt(encoded[i - 1] | c >> 4);
-            encoded[i + 1] = c << 4;
         }
+
+        if (bufIdx == 2) {
+            encoded[encodedIdx] = self.charAt(buf[0] >> 2);
+            encoded[encodedIdx + 1] = self.charAt(((buf[0] & 0x03) << 4) | buf[1] >> 4);
+            encoded[encodedIdx + 2] = self.charAt((buf[1] & 0x0f) << 2);
+            encoded[encodedIdx + 3] = '=';
+
+            encodedIdx += 4;
+            bufIdx = 0;
+        }
+
+        if (bufIdx == 1) {
+            encoded[encodedIdx] = self.charAt(buf[0] >> 2);
+            encoded[encodedIdx + 1] = self.charAt((buf[0] & 0x03) << 4);
+            encoded[encodedIdx + 2] = '=';
+            encoded[encodedIdx + 3] = '=';
+
+            encodedIdx += 4;
+            bufIdx = 0;
+        }
+
+        assert(encodedIdx == encodedTextLen);
+
+        return encoded;
     }
 };
 
-pub fn main() !void {}
+pub fn main() !void {
+    var gpa = heap.GeneralPurposeAllocator(.{ .thread_safe = true }){};
+    const allocator = gpa.allocator();
+    defer if (gpa.deinit() == .leak) {
+        print("Memory leak", .{});
+    };
+
+    const base64Encoder = base64.init();
+    const input = "Man";
+    const encoded = base64Encoder.encode(allocator, input) catch |err| {
+        print("There was an error while encoding:{any}\n", .{err});
+        return;
+    };
+    defer allocator.free(encoded);
+
+    print("The base64 encoded string of '{s}' is '{s}'\n", .{ input, encoded });
+}
